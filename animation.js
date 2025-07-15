b18da396125f5215bbf803461d6b5120651d09e4
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-15 22:28:15 +0530

    Update animation.js
    seeing if that whole const thing is working

--- animation.js
+++ animation.js
@@ -1,12 +1,4 @@
 function mergeFrames() {
-  const script = `
-    (function () {
-      var doc = app.activeDocument;
-      var newLayer = doc.createLayer();
-      newLayer.name = "Layer_From_Plugin";
-      alert("✅ New layer created by plugin.");
-    })();
-  `;
-
-  window.parent.postMessage(script.trim(), "*");
+  const script = `alert("🧪 Hello from plugin - basic test")`;
+  window.parent.postMessage(script, "*");
 }
