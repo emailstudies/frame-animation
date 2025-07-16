f88d0bba515e6a1a43ead564aec77fced94e19bc
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-16 20:44:14 +0530

    Create app.js
    created the js to code what executes when a button is clicked.
    handlers in the actual js itself

---
+++ src/app.js
@@ -0,0 +1,6 @@
+document.getElementById("createAnimBtn").onclick = handleAddAnimation;
+
+// Placeholders for later phases
+document.getElementById("mapFramesBtn").onclick = () => alert("Feature coming soon!");
+document.getElementById("mergePreviewBtn").onclick = () => alert("Feature coming soon!");
+document.getElementById("exportGifBtn").onclick = () => alert("Feature coming soon!");
