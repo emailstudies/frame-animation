756903f8117c36df3727f06b91e2153e73744ec4
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-16 20:49:35 +0530

    Update make_animation.js
    checking if only 1 layer can be made

--- src/make_animation.js
+++ src/make_animation.js
@@ -1,43 +1,16 @@
 function handleAddAnimation() {
-  const userInput = document.getElementById("animFolderInput").value.trim();
-  if (!userInput) {
-    alert("Please enter a folder name.");
-    return;
-  }
-
-  const folderName = `anim_${userInput}`;
-
   const script = `
-    // Create a group named "${folderName}"
-    var desc1 = new ActionDescriptor();
-    var ref1 = new ActionReference();
-    ref1.putClass(stringIDToTypeID("layerSection"));
-    desc1.putReference(charIDToTypeID("null"), ref1);
+    var desc = new ActionDescriptor();
+    var ref = new ActionReference();
+    ref.putClass(charIDToTypeID("Lyr "));
+    desc.putReference(charIDToTypeID("null"), ref);
 
-    var desc2 = new ActionDescriptor();
-    desc2.putString(charIDToTypeID("Nm  "), "${folderName}");
-    desc1.putObject(charIDToTypeID("Usng"), stringIDToTypeID("layerSection"), desc2);
+    var layerProps = new ActionDescriptor();
+    layerProps.putString(charIDToTypeID("Nm  "), "Frame 1");
 
-    app.doAction("make", desc1);
+    desc.putObject(charIDToTypeID("Usng"), charIDToTypeID("Lyr "), layerProps);
 
-    // Select the newly created group
-    var group = app.activeDocument.activeLayer;
-
-    // Create a new layer named "Frame 1"
-    var desc3 = new ActionDescriptor();
-    var ref2 = new ActionReference();
-    ref2.putClass(charIDToTypeID("Lyr "));
-    desc3.putReference(charIDToTypeID("null"), ref2);
-
-    var desc4 = new ActionDescriptor();
-    desc4.putString(charIDToTypeID("Nm  "), "Frame 1");
-    desc3.putObject(charIDToTypeID("Usng"), charIDToTypeID("Lyr "), desc4);
-
-    app.doAction("make", desc3);
-
-    // Move the layer into the group
-    var newLayer = app.activeDocument.activeLayer;
-    newLayer.move(group, ElementPlacement.INSIDE);
+    app.doAction("make", desc);
   `;
 
   window.parent.postMessage(script, "*");
