0364bca6105f71fab184c9affe406eff9c168019
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-16 20:48:10 +0530

    Update make_animation.js
    using action descriptors to create folder/layer
    
    Previously - We can't reliably use group.addLayer("Frame 1") in Photopea scripting. That syntax mirrors Photoshop’s JS DOM, but Photopea requires Action Descriptors (app.doAction(...)) for many operations like creating layers inside groups, especially when automating via plugins.
    
    We discussed earlier that creating layers/folders in Photopea must be done using action descriptors, not via JS DOM-style objects like layerTree.addLayerGroup() or group.addLayer().

--- src/make_animation.js
+++ src/make_animation.js
@@ -6,10 +6,38 @@ function handleAddAnimation() {
   }
 
   const folderName = `anim_${userInput}`;
+
   const script = `
-    var doc = app.activeDocument;
-    var group = doc.layerTree.addLayerGroup("${folderName}");
-    group.addLayer("Frame 1");
+    // Create a group named "${folderName}"
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putClass(stringIDToTypeID("layerSection"));
+    desc1.putReference(charIDToTypeID("null"), ref1);
+
+    var desc2 = new ActionDescriptor();
+    desc2.putString(charIDToTypeID("Nm  "), "${folderName}");
+    desc1.putObject(charIDToTypeID("Usng"), stringIDToTypeID("layerSection"), desc2);
+
+    app.doAction("make", desc1);
+
+    // Select the newly created group
+    var group = app.activeDocument.activeLayer;
+
+    // Create a new layer named "Frame 1"
+    var desc3 = new ActionDescriptor();
+    var ref2 = new ActionReference();
+    ref2.putClass(charIDToTypeID("Lyr "));
+    desc3.putReference(charIDToTypeID("null"), ref2);
+
+    var desc4 = new ActionDescriptor();
+    desc4.putString(charIDToTypeID("Nm  "), "Frame 1");
+    desc3.putObject(charIDToTypeID("Usng"), charIDToTypeID("Lyr "), desc4);
+
+    app.doAction("make", desc3);
+
+    // Move the layer into the group
+    var newLayer = app.activeDocument.activeLayer;
+    newLayer.move(group, ElementPlacement.INSIDE);
   `;
 
   window.parent.postMessage(script, "*");
