7550f165477611508e10ccd3d2c54fec749093ae
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-20 18:15:26 +0530

    Update onion_skin.js
    previous one was changing opacity of the current layer and not the previous and after

--- src/onion_skin.js
+++ src/onion_skin.js
@@ -1,27 +1,34 @@
-function toggleOnionSkinMode() {
+function applyOnionSkinOnce() {
   const script = `
     var doc = app.activeDocument;
     var sel = doc.activeLayer;
-
+    
     if (sel && sel.typename !== "LayerSet") {
+      var selName = sel.name;
       var parent = sel.parent;
+
       if (parent && parent.typename === "LayerSet") {
         var siblings = parent.layers;
-        var idx = siblings.indexOf(sel);
+        var idx = -1;
 
-        // Previous layer
-        if (idx > 0) {
-          var prev = siblings[idx - 1];
-          if (prev.typename !== "LayerSet") {
-            prev.opacity = 40;
+        for (var i = 0; i < siblings.length; i++) {
+          if (siblings[i].name === selName && siblings[i].typename !== "LayerSet") {
+            idx = i;
+            break;
           }
         }
 
-        // Next layer
-        if (idx < siblings.length - 1) {
-          var next = siblings[idx + 1];
-          if (next.typename !== "LayerSet") {
-            next.opacity = 40;
+        if (idx > -1) {
+          // Previous layer
+          if (idx > 0) {
+            var prev = siblings[idx - 1];
+            if (prev.typename !== "LayerSet") prev.opacity = 40;
+          }
+
+          // Next layer
+          if (idx < siblings.length - 1) {
+            var next = siblings[idx + 1];
+            if (next.typename !== "LayerSet") next.opacity = 40;
           }
         }
       }
