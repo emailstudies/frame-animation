8607b064be211dd5ebbe8666641975d28e2a9e7a
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-20 20:31:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,15 @@ document.getElementById("createAnimBtn").onclick = handleCreateFolder;
 // Placeholders for later phases
 document.getElementById("updateLayerNumbersBtn").onclick = handleUpdateLayerNumbers;
 
-document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode; //single step
+document.getElementById("onionSkinBtn").onclick = () => {
+  if (!window.onionSkinMode) {
+    window.toggleOnionSkinMode(); // Enable mode
+  } else {
+    window.handleLiveOnionSkin(); // Apply on newly selected layer
+  }
+};
+
+// document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode; //single step
 
 // Onion Skin toggle mode
 /*document.getElementById("onionSkinBtn").onclick = () => {
