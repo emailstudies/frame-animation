42f571fbc2fe2e56f390f65ef1c956c2fc48d666
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-20 14:55:07 +0530

    Update update_layer_numbers.js
    checking structure of the folder

--- src/update_layer_numbers.js
+++ src/update_layer_numbers.js
@@ -1,44 +1,14 @@
 function handleUpdateLayerNumbers() {
   const script = `
     var doc = app.activeDocument;
+    var msg = "";
 
-    // Find first anim_* folder
-    var animFolder = null;
     for (var i = 0; i < doc.layers.length; i++) {
       var folder = doc.layers[i];
-      if (folder.isGroup && folder.name.indexOf("anim_") === 0) {
-        animFolder = folder;
-        break;
-      }
+      msg += "Layer " + i + ": " + folder.name + " | isGroup: " + folder.isGroup + "\\n";
     }
 
-    if (!animFolder) {
-      alert("No anim_ folder found.");
-    } else {
-      // Collect non-group layers only
-      var frameLayers = [];
-      for (var j = 0; j < animFolder.layers.length; j++) {
-        var layer = animFolder.layers[j];
-        if (!layer.isGroup) frameLayers.push(layer);
-      }
-
-      var max = frameLayers.length;
-      if (max === 0) {
-        alert("No layers inside anim folder.");
-      } else {
-        var baseName = frameLayers[max - 1].name;
-
-        // Rename top → bottom: 3/3, 2/3, 1/3
-        for (var k = 0; k < max; k++) {
-          var frameNum = max - k;
-          try {
-            frameLayers[k].name = frameNum + "/" + max + " " + baseName;
-          } catch (e) {}
-        }
-
-        alert("Layer Numbers Updated");
-      }
-    }
+    alert(msg);
   `;
 
   window.parent.postMessage(script, "*");
