108680cacaaeffe98929e3f2cb9b0302fc87499e
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-20 17:28:47 +0530

    Update app.js
    added onionSkin.js

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ document.getElementById("createAnimBtn").onclick = handleCreateFolder;
 
 // Placeholders for later phases
 document.getElementById("updateLayerNumbersBtn").onclick = handleUpdateLayerNumbers;
-document.getElementById("mergePreviewBtn").onclick = () => alert("Feature coming soon!");
+document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode;
 document.getElementById("exportGifBtn").onclick = () => alert("Feature coming soon!");
