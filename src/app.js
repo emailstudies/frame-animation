acb1696e7d2fde110e0f1c5052abbe99628b50de
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-20 14:06:55 +0530

    Update app.js
    there was this => ()
    gave an error

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 document.getElementById("createAnimBtn").onclick = handleCreateFolder;
 
 // Placeholders for later phases
-document.getElementById("updateLayerNumbersBtn").onclick = () => handleUpdateLayerNumbers;
+document.getElementById("updateLayerNumbersBtn").onclick = handleUpdateLayerNumbers;
 document.getElementById("mergePreviewBtn").onclick = () => alert("Feature coming soon!");
 document.getElementById("exportGifBtn").onclick = () => alert("Feature coming soon!");
