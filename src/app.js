4506a4be2fc68ea2ccc9da654907ba29e40bded0
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-20 22:51:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,4 @@
 document.getElementById("createAnimBtn").onclick = handleCreateFolder;
-
-// Placeholders for later phases
 document.getElementById("updateLayerNumbersBtn").onclick = handleUpdateLayerNumbers;
-
-
-/*document.getElementById("onionSkinBtn").onclick = () => {
-  if (!window.onionSkinMode) {
-    window.toggleOnionSkinMode(); // Enable mode
-  } else {
-    window.handleLiveOnionSkin(); // Apply on newly selected layer
-  }
-}; 
-*/
-
- document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode; //single step
-
-// Onion Skin toggle mode
-/*document.getElementById("onionSkinBtn").onclick = () => {
-  window.toggleOnionSkinMode();
-};*/
+document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode; 
 document.getElementById("exportGifBtn").onclick = () => alert("Feature coming soon!");
-
-// onion skin
-
-/* document.getElementById("onionSkinBtn").onclick = () => {
-  window.toggleOnionSkinMode(1); // Change 2 to 1–5 for step depth
-}; */
