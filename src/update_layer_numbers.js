860701d4cc864b7bcd115738dac7aeb223d9c0b1
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-20 15:00:32 +0530

    Update update_layer_numbers.js
    so isGroup was returned as undefines so isGroup does not identif features.
    so using LayerSets

--- src/update_layer_numbers.js
+++ src/update_layer_numbers.js
@@ -1,14 +1,44 @@
 function handleUpdateLayerNumbers() {
   const script = `
     var doc = app.activeDocument;
-    var msg = "";
 
+    // Find first folder that starts with anim_
+    var animFolder = null;
     for (var i = 0; i < doc.layers.length; i++) {
       var folder = doc.layers[i];
-      msg += "Layer " + i + ": " + folder.name + " | isGroup: " + folder.isGroup + "\\n";
+      if (folder.name && folder.name.startsWith("anim_") && folder.typename === "LayerSet") {
+        animFolder = folder;
+        break;
+      }
     }
 
-    alert(msg);
+    if (!animFolder) {
+      alert("No anim_ folder found.");
+    } else {
+      var frameLayers = [];
+
+      for (var j = 0; j < animFolder.layers.length; j++) {
+        var layer = animFolder.layers[j];
+        if (layer.typename !== "LayerSet") frameLayers.push(layer);
+      }
+
+      var max = frameLayers.length;
+      if (max === 0) {
+        alert("No frame layers inside anim folder.");
+      } else {
+        var baseName = frameLayers[max - 1].name;
+
+        // Rename top to bottom: 3/3, 2/3, 1/3, etc.
+        for (var k = 0; k < max; k++) {
+          var frameNum = max - k;
+          try {
+            frameLayers[k].name = frameNum + "/" + max + " " + baseName;
+          } catch (e) {}
+        }
+
+        alert("Layer Numbers Updated");
+      }
+    }
   `;
 
   window.parent.postMessage(script, "*");
