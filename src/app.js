8d0263fd6ebb17ba5d72821063b53aca26779dcf
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-21 21:12:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 document.getElementById("createAnimBtn").onclick = handleCreateFolder;
 document.getElementById("updateLayerNumbersBtn").onclick = handleUpdateLayerNumbers;
 document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode; 
+document.getElementById("resetOnionSkinBtn").onclick = resetOnionSkin; 
 document.getElementById("exportGifBtn").onclick = () => alert("Feature coming soon!");
 
