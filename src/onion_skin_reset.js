34d3e83a4c3e441cab440d33f413d32575c0a6d9
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-21 21:11:05 +0530

    Create onion_skin_reset.js

---
+++ src/onion_skin_reset.js
@@ -0,0 +1,41 @@
+function resetOnionSkin() {
+  const script = `
+    (function () {
+      var doc = app.activeDocument;
+      if (!doc) {
+        alert("No active document.");
+        return;
+      }
+
+      // Loop through all anim_* folders
+      for (var i = 0; i < doc.layers.length; i++) {
+        var group = doc.layers[i];
+        if (group.typename === "LayerSet" && group.name.startsWith("anim_")) {
+          var layers = group.layers;
+          if (layers.length === 0) continue;
+
+          // Topmost layer gets 100% opacity
+          try {
+            if (layers[0].typename !== "LayerSet") {
+              layers[0].opacity = 100;
+            }
+          } catch (e) {}
+
+          // All other layers get 0% opacity
+          for (var j = 1; j < layers.length; j++) {
+            var layer = layers[j];
+            if (layer.typename !== "LayerSet") {
+              try {
+                layer.opacity = 0;
+              } catch (e) {}
+            }
+          }
+        }
+      }
+
+      alert("🧼 Onion Skin Reset: Topmost layer of each anim_* folder is now visible.");
+    })();
+  `;
+
+  window.parent.postMessage(script, "*");
+}
