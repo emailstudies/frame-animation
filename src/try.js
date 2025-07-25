0aeaf13aad30c028e117c89e9a4286f69b35b0a9
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-25 13:32:40 +0530

    Update try.js
    trying to see if this orks

--- src/try.js
+++ src/try.js
@@ -21,38 +21,50 @@ function exportGif() {
     return;
   }
 
-  // Step 2: Create new document with same size as original
-  var width = originalDoc.width;
-  var height = originalDoc.height;
+  // Step 2: Make all Frame_ layers visible for duplication
+  for (var i = 0; i < frameLayers.length; i++) {
+    if (!frameLayers[i].allLocked) {
+      frameLayers[i].visible = true;
+    }
+  }
+
+  // Step 3: Duplicate each frame and store references
+  var duplicatedLayers = [];
+  for (var i = 0; i < frameLayers.length; i++) {
+    var dup = frameLayers[i].duplicate();
+    duplicatedLayers.push(dup);
+  }
+
+  // Step 4: Get bounds from top frame (assumes same size)
+  var refLayer = duplicatedLayers[0];
+  var width = refLayer.bounds[2] - refLayer.bounds[0];
+  var height = refLayer.bounds[3] - refLayer.bounds[1];
+
+  // Step 5: Create new doc for preview
   var previewDoc = app.documents.add(width, height, 72, "Animation Preview", NewDocumentMode.RGB);
 
-  // Step 3: Copy + paste each frame layer (group or normal)
-  for (var i = frameLayers.length - 1; i >= 0; i--) {
-    var originalLayer = frameLayers[i];
-
-    // Switch to original doc
+  // Step 6: Paste each duplicated layer into new doc
+  for (var i = duplicatedLayers.length - 1; i >= 0; i--) {
     app.activeDocument = originalDoc;
-
-    // Make visible if not locked
-    if (!originalLayer.allLocked) {
-      originalLayer.visible = true;
-    }
-
-    app.activeDocument.activeLayer = originalLayer;
+    duplicatedLayers[i].visible = true;
+    app.activeDocument.activeLayer = duplicatedLayers[i];
     app.runMenuItem(stringIDToTypeID("copy"));
 
-    // Switch to preview doc and paste
     app.activeDocument = previewDoc;
     app.runMenuItem(stringIDToTypeID("paste"));
 
     var pasted = previewDoc.activeLayer;
-    pasted.name = "_a_" + originalLayer.name;
+    pasted.name = "_a_" + frameLayers[i].name;
     pasted.visible = visibilityStatus[i];
     if (lockedStatus[i]) pasted.allLocked = true;
   }
 
-  // Step 4: Switch back to original doc
+  // Step 7: Return to original doc and clean up temp layers
   app.activeDocument = originalDoc;
+  for (var i = 0; i < duplicatedLayers.length; i++) {
+    duplicatedLayers[i].remove();
+  }
+
 })();`.trim();
 
   window.parent.postMessage(script, "*");
