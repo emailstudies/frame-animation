83c81f253989af7e7af65cdbbce9223d76b7dd97
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-30 12:46:27 +0530

    Update send_images.js

--- src/send_images.js
+++ src/send_images.js
@@ -1,5 +1,13 @@
 function openMagicCut() {
+  const script = `app.showWindow("export");`;
+  window.parent.postMessage(script, "*");
+  console.log("📤 Sent request to open Magic Cut");
+}
+
+/*
+function openMagicCut() {
   const script = `app.showWindow("magiccut");`;
   window.parent.postMessage(script, "*");
   console.log("📤 Sent request to open Magic Cut");
 }
+*/
