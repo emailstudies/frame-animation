734f9c02da242db41773979abe8582ab606bfe26
emailstudies <mir.s.jani.courses@gmail.com>
2025-07-30 22:49:41 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-document.getElementById("createAnimBtn").onclick = handleCreateFolder;
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("createAnimBtn").onclick = handleCreateFolder;
+  document.getElementById("updateLayerNumbersBtn").onclick = handleUpdateLayerNumbers;
+  document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode; 
+  document.getElementById("resetOnionSkinBtn").onclick = resetOnionSkin; 
+  document.getElementById("previewAllBtn").onclick = exportGif;
+});
+// wrapping in a function because now app.js is being loaded first in index.html because of fps
+
+
+/*document.getElementById("createAnimBtn").onclick = handleCreateFolder;
 document.getElementById("updateLayerNumbersBtn").onclick = handleUpdateLayerNumbers;
 document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode; 
 document.getElementById("resetOnionSkinBtn").onclick = resetOnionSkin; 
