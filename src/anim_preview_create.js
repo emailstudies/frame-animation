d89e53363a92833d6de79f11b32b8243fd96120d
emailstudies <mir.s.jani.courses@gmail.com>
2025-08-03 13:23:31 +0530

    Update anim_preview_create.js
    app.echoToOE("✅ anim_preview created - done"); - added

--- src/anim_preview_create.js
+++ src/anim_preview_create.js
@@ -269,6 +269,7 @@ function exportGif() {
       app.refresh();
       
       alert("✅ All frames merged into 'anim_preview'. You can now export via File > Export As > GIF.");
+      app.echoToOE("✅ anim_preview created - done");
 
       
       
