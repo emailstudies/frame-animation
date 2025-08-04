18d1f303d715ad113c0de9b4e4475b2fa02fb0d3
emailstudies <mir.s.jani.courses@gmail.com>
2025-08-04 17:29:58 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -36,6 +36,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 };
 
+  launchFlipbookFromAnimPreview();
+
+
 
 document.getElementById("browserPreviewAllBtn").onclick = () => {
   const script = `
