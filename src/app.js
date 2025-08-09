66d1d2c88f004e7b23e4c90b8fa49e04e9b7c389
emailstudies <mir.s.jani.courses@gmail.com>
2025-08-09 19:40:31 +0530

    Update app.js
    changed reset onion skin to the beforeMergeInExport

--- src/app.js
+++ src/app.js
@@ -9,13 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("createAnimBtn").onclick = handleCreateFolder;
   document.getElementById("updateLayerNumbersBtn").onclick = handleUpdateLayerNumbers;
  /* document.getElementById("onionSkinBtn").onclick = toggleOnionSkinMode; */
-  document.getElementById("resetOnionSkinBtn").onclick = resetOnionSkin;
+
+  /* this was the previous onion skin logic which make all 1005 but turened on visibility of only Layer 1 in folder as true 
+  document.getElementById("resetOnionSkinBtn").onclick = resetOnionSkin; */
+
+  /* I prefer this for the reset since it will make everything opacity as 100 and visible so easier to navigate */
+  document.getElementById("resetOnionSkinBtn").onclick = beforeMergingInExport;
   /* document.getElementById("browserPreviewAllBtn").onclick = previewInPhotopeaFlipbook; ths was app.refresh to update canvas in PP itself */
 
 
   /* doing the reset first before the onion skin happens */
   document.getElementById("onionSkinBtn").onclick = function () {
-  resetOnionSkin(); // reset first
+  beforeMergingInExport(); // reset first
   setTimeout(() => {
     const before = parseInt(document.getElementById("beforeSteps").value, 10);
     const after = parseInt(document.getElementById("afterSteps").value, 10);
